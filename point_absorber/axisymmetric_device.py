21d7c0dcc8d4bd842fa5aa3ea7e7418c27c092f7
Austin Berrier <55550076+berriera@users.noreply.github.com>
2021-03-13 22:40:34 -0800

    Update axisymmetric_device.py
    - Added finding discrete starting point to random hill climbing algorithm
    - Internal loop only runs random hill climbing algorithm now

--- point_absorber/axisymmetric_device.py
+++ point_absorber/axisymmetric_device.py
@@ -70,12 +70,12 @@ def make_mesh(points_array):
     mass = volume * 1000
     radius = points_array[-1][0]
     stiffness = np.pi * 1000 * 9.81 * radius ** 2
-    approximate_resonance_radial_frequency = np.sqrt(stiffness / mass)
+    #approximate_resonance_radial_frequency = np.sqrt(stiffness / mass)
 
-    if verbose:
-        print('\tMass = {}'.format(mass))
-        print('\tStiffness = {}'.format(stiffness))
-        print('\tApproximate resonance radial frequency = {} rad/s'.format(approximate_resonance_radial_frequency))
+    #if verbose:
+    #    print('\tMass = {}'.format(mass))
+    #    print('\tStiffness = {}'.format(stiffness))
+    #    print('\tApproximate resonance radial frequency = {} rad/s'.format(approximate_resonance_radial_frequency))
 
     return mesh_shape, buoy, mass, stiffness
 
@@ -230,7 +230,9 @@ def exhaustive_search(lower_bound, upper_bound, delta_x):
 
     return x_best, f_best
 
-def random_hill_climbing_algorithm(x_initial_option):
+def random_hill_climbing_algorithm(lower_bounds, upper_bounds, delta_x, random_seed):
+    import random
+
     # Define transition function
     def random_climb(x, transitions, lower_bound, upper_bound):
         in_bounds = False
@@ -244,12 +246,20 @@ def random_hill_climbing_algorithm(x_initial_option):
 
         return x
 
+    # Find a random discrete starting point within the domain bounds if one is not given
+    d = len(delta_x)
+    if random_seed is not None:
+        random.seed(random_seed)
+    x_0 = np.zeros(shape=d)
+    for k in range(d):
+        n = int((upper_bounds[k] - lower_bounds[k]) / delta_x[k]) + 1
+        x_0[k] = lower_bounds[k] + random.randint(0, n) * delta_x[k]
+
     # Define tunable parameters
     increment_options = np.array([0.50, -0.50, 0.25, -0.25])
-    bounds = [0.01, 5]
-    max_failed_moves = 32
+    bounds = [0.01, 5.0]
+    max_failed_moves = 128
 
-    x_0 = x_initial_option
     k = 0
     converged = False
     f_0 = objective_function(x_0)
@@ -260,7 +270,7 @@ def random_hill_climbing_algorithm(x_initial_option):
         x_new = random_climb(x_k, increment_options, bounds[0], bounds[1])
         f_new = objective_function(x_new)
         if f_new <= f_k:
-            x_k = x_new
+            x_k = np.copy(x_new)
             f_k = f_new
             failed_moves = 0
         else:
@@ -297,7 +307,7 @@ if __name__ == '__main__':
 
     # User input: Optimization Vars
     ###opt_method = 'Nelder-Mead'
-    number_of_runs = 4
+    number_of_runs = 30
     max_iterations = None  # Set to None if you want the default max iterations
     ##############################
 
@@ -307,7 +317,7 @@ if __name__ == '__main__':
     for run in range(number_of_runs):
     
         # Global List used to record all profiles
-        ###point_history = []
+        point_history = []
 
         # Random seed for reprodicible initial starting points
         ###np.random.seed(run * radial_control_points)
@@ -327,27 +337,24 @@ if __name__ == '__main__':
         #                  start_x_points,
         #                  method=opt_method,
         #                  options={'disp': True, 'return_all': True, 'maxiter': max_iterations})
-        if run == 0:
-            point_history = []
-            radial_control_points = 2
-            z_control_pts = np.linspace(-draft, 0, radial_control_points)
-            opt_method = 'exhaustive_search'
-            lower_bound = 0.25*np.ones(shape=2)
-            upper_bound = 5.0*np.ones(shape=2)
-            delta_x = 0.25*np.ones(shape=2)
-            exhaustive_search(lower_bound=lower_bound, upper_bound=upper_bound, delta_x=delta_x)
+        #if run == 0:
+        #    point_history = []
+        #    radial_control_points = 2
+        #    z_control_pts = np.linspace(-draft, 0, radial_control_points)
+        #    opt_method = 'exhaustive_search'
+        #    lower_bound = 0.25*np.ones(shape=2)
+        #    upper_bound = 5.0*np.ones(shape=2)
+        ##    delta_x = 0.25*np.ones(shape=2)
+        #exhaustive_search(lower_bound=lower_bound, upper_bound=upper_bound, delta_x=delta_x)
 
-        else:
-            point_history = []
-            radial_control_points = 4
-            z_control_pts = np.linspace(-draft, 0, radial_control_points)
-            opt_method = 'random_hill_climbing_algorithm'
-            x_initial_options = np.array([[1.,  4.5, 0.5, 2. ],
-                                         [0.25, 0.5,  0.5,  2.75],
-                                         [1.75, 4.5,  4.25, 1.  ]]
-                                         )
-            x_start = x_initial_options[run - 1]
-            random_hill_climbing_algorithm(x_start)
+        lower_bounds = 0.25*np.ones(shape=4)
+        upper_bounds = 5.0*np.ones(shape=4)
+        delta_x = 0.25*np.ones(shape=4)
+
+        radial_control_points = 4
+        z_control_pts = np.linspace(-draft, 0, radial_control_points)
+        opt_method = 'random_hill_climbing_algorithm'
+        random_hill_climbing_algorithm(lower_bounds=lower_bounds, upper_bounds=upper_bounds, delta_x=delta_x, random_seed=run)
 
         end_time = time.perf_counter()
 
